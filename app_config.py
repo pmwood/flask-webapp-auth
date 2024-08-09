a13cc737ac8ebbe062dd7c0ec20da56469af2e8c
pmwood <pmwood@gmail.com>
2024-08-09 14:36:04 -0400

    Update app_config.py

--- app_config.py
+++ app_config.py
@@ -1,27 +1,9 @@
 import os
 
-
-if (os.getenv('B2C_TENANT_NAME')
-    and os.getenv('SIGNUPSIGNIN_USER_FLOW') and os.getenv('EDITPROFILE_USER_FLOW')):
-    # This branch is for B2C. You can delete this branch if you are not using B2C.
-    b2c_tenant = os.getenv('B2C_TENANT_NAME')
-    authority_template = "https://{tenant}.b2clogin.com/{tenant}.onmicrosoft.com/{user_flow}"
-    AUTHORITY = authority_template.format(
-        tenant=b2c_tenant,
-        user_flow=os.getenv('SIGNUPSIGNIN_USER_FLOW'))
-    B2C_PROFILE_AUTHORITY = authority_template.format(
-        tenant=b2c_tenant,
-        user_flow=os.getenv('EDITPROFILE_USER_FLOW'))
-    B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(
-        # If you are using the new "Recommended user flow"
-        # (https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-versions),
-        # you can remove the B2C_RESET_PASSWORD_AUTHORITY settings from this file.
-        tenant=b2c_tenant,
-        user_flow=os.getenv('RESETPASSWORD_USER_FLOW'))
-else:  # This branch is for AAD or CIAM
-    # You can configure your authority via environment variable
-    # Defaults to a multi-tenant app in world-wide cloud
-    AUTHORITY = os.getenv("AUTHORITY") or "https://login.microsoftonline.com/common"
+# This branch is for AAD or CIAM
+# You can configure your authority via environment variable
+# Defaults to a multi-tenant app in world-wide cloud
+AUTHORITY = os.getenv("AUTHORITY") or "https://login.microsoftonline.com/common"
 
 # Application (client) ID of app registration
 CLIENT_ID = os.getenv("CLIENT_ID")
